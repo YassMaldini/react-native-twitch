6f28cdb812b156f19b3c4ee8495aea9457d70ac3
YassMaldini <yass.maldini@gmail.com>
2023-01-14 12:06:34 +0100

    chore: add hermes and filpper handling

--- src/components/commons/Application/Application.tsx
+++ src/components/commons/Application/Application.tsx
@@ -4,12 +4,21 @@ import { Provider as StoreProvider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import Navigation from '../../navigation/Navigation';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+// @ts-ignore
+import { connectToDevTools } from 'react-devtools-core';
 
 import '../../../utils/i18n/i18n';
 
 const Application = () => {
   const { store, persistor } = configureStore();
   const queryClient = new QueryClient();
+
+  if (__DEV__) {
+    connectToDevTools({
+      host: 'localhost',
+      port: 8097,
+    });
+  }
 
   return (
     <StoreProvider {...{ store }}>
